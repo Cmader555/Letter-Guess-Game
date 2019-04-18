7e4d48b4fe768fb4fbc8bac5c9be73a2073cbb54
Christian Mader <Cmader555@users.noreply.github.com>
2019-04-18 09:44:55 -0700

    NOW ITS FINALLY FINISHED

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,21 +3,35 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "Z"]
 
 //variables that will be altered and effects will be seen on screen
-var wins = 0
-var losses = 0
-var guessesLeft = 5
-var lettersChosen = []
-
+var wins = 0;
+var losses = 0;
+var guessesLeft = 5;
+var lettersChosen = [];
+var computerGuess;
 //grabbing html content so I can later change it with javascript
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guessesLeft-text");
 var lettersChosenText = document.getElementById("lettersChosen-text");
+var img = document.getElementById("theImg");
 
 //randomly generates
-var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-console.log(computerGuess);
+
+
+function intiate() {
+
+    computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    console.log(computerGuess);
+
+    guessesLeft = 5;
+    lettersChosen = [];
+
+}
+
+intiate();
+
 
 //key is pressed and key value compared to random guess
 document.onkeyup = function (event) {
@@ -28,27 +42,21 @@ document.onkeyup = function (event) {
 
         if (userGuess === computerGuess) {
             wins++;
-            guessesLeft = 5;
-            lettersChosen = [];
-            document.getElementById("theImg").src = "assets/images/simpson-doughnut.png";
-        }
-
-
-        if (userGuess !== computerGuess) {
+            img.src = "assets/images/simpson-doughnut.png";
+            intiate();
+        } else if (userGuess !== computerGuess) {
             guessesLeft--;
-            document.getElementById("theImg").src = "assets/images/homer-doh.png";
-
+            img.src = "assets/images/homer-doh.png";
         }
-
 
     }
 
 
     if (guessesLeft === 0) {
         losses++;
-        guessesLeft = 5;
-        lettersChosen = [];
-        document.getElementById("theImg").src = "assets/images/homer-lose.gif";
+        img.src = "assets/images/homer-lose.gif";
+        intiate();
+
     }
 
 
@@ -57,6 +65,7 @@ document.onkeyup = function (event) {
     guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
     lettersChosenText.textContent = "Letters Chosen So Far: " + lettersChosen;
 }
+
 
 //This is here so text is there when page is loaded 
 winsText.textContent = "Wins: " + wins;
